c80b3de20fc46b663790a64007a5a5afd0e15ca7
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2026-02-02 08:22:29 +0600

    chore: remove php and docker stack

--- contracts/Exceptions/GitHubProfileViewsCounterThrowableInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Contracts\App\GitHubProfileViewsCounter\Exceptions;
-
-use Throwable;
-
-interface GitHubProfileViewsCounterThrowableInterface extends Throwable {}
